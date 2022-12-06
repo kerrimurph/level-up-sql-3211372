589a388985de5f39bc00e7f605917eab17ca6cd7
kerrimurph <118017534+kerrimurph@users.noreply.github.com>
2022-12-06 19:50:31 +0000

    CommitTest01

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,19 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+
+SELECT * FROM Customers
+WHERE Email = 'atapley2j@kinetecoinc.com'
+
+SELECT * FROM Reservations
+WHERE CustomerId = 92
+
+INSERT INTO AnniversaryAttendees 
+  (CustomerId, NumberOfPeople)
+VALUES (
+  (SELECT CustomerID
+    FROM Customers
+    WHERE Email = 'atapley2j@kinetecoinc.com'),
+  4);
+
+SELECT * FROM AnniversaryAttendees
--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,18 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+SELECT *
+FROM Reservations
+
+SELECT * FROM Customers
+WHERE LastName LIKE 'St%'
+
+
+Select C.FirstName,C.LastName,C.Email,R.ReservationId,R.Date,R.PartySize
+FROM Customers C
+INNER JOIN Reservations R
+ON C.CustomerId = R.CustomerId
+WHERE R.PartySize = 4
+AND C.LastName LIKE 'St%'
+AND R.Date > '2022-06-14'
